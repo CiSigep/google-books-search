bac4039530bc25ec532a4d6d9cf631d475ab5f53
CiSigEp <cisigep@gmail.com>
2019-09-13 19:50:22 -0500

    Initial setup

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({running:  true});
+});
+
+
+app.listen(PORT, () => {
+  console.log("App listening on port: " + PORT);
+});
